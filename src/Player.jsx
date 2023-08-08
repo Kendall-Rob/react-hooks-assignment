c5bd51c096515037f094e1f744bdae88085f0678
Kendall Robinson <kentechs91@gmail.com>
2023-08-08 11:14:17 -0400

    Add changes as directed in exercise

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { useState } from "react";
-function Player() {
+function Player(player) {
+    let [points, addPoints] = useState(0);
+
   return (
     <div className="container-fluid border border-dark p-3 m-3">
       <div className="row justify-content-center">
-        {/* Render Name here */}
+        {player.name}
         <h2 className="text-center"></h2>
       </div>
       <div className="row justify-content-center">
-        <p className="text-center fs-3">{/* Render score here */}</p>
+        <p className="text-center fs-3">{points}</p>
       </div>
       <div className="row justify-content-center">
         <div className="col-md-6">
           {/* Add increment event handler */}
-          <button className="btn btn-outline-primary w-100">Add Point +</button>
+          <button onClick={() => addPoints(points++)} className="btn btn-outline-primary w-100">Add Point +</button>
         </div>
         <div className="col-md-6">
           {/* Add decrement event handler */}
-          <button className="btn btn-outline-danger w-100">
+          <button onClick={() => addPoints(points--)} className="btn btn-outline-danger w-100">
             Remove Point -
           </button>
         </div>
--- src/ScoreBoard.jsx
+++ src/ScoreBoard.jsx
@@ -2,8 +2,18 @@ import { useRef, useState } from "react";
 //Bootstrap has already been downloaded for you
 import "bootstrap/dist/css/bootstrap.min.css";
 import Player from "./Player";
+import React from "react";
+
+
 function ScoreBoard() {
- 
+    let [players, setPlayers] = useState([]);
+    let inputRef = useRef();
+
+    function addPlayers() {
+      let playerInputText = inputRef.current.value || undefined;
+      setPlayers([...players, {name: playerInputText, id: Date.now()}]);
+    }
+
   return (
     <div className="container">
       <div className="row  text-center">
@@ -14,6 +24,7 @@ function ScoreBoard() {
           <div class="input-group mb-3">
             {/* Modify input so that it is either connected to a ref or some kind of input state */}
             <input
+            ref={inputRef}
               type="text"
               class="form-control"
               placeholder="New Player Name"
@@ -23,6 +34,7 @@ function ScoreBoard() {
             />
             {/* add Add Player event handling to this button */}
             <button
+              onClick={addPlayers}
               class="btn btn-outline-primary"
               type="button"
               id="addPlayer"
